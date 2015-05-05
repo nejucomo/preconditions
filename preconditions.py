38b4af0b3c1c6105d68ff453d86107758ef9d751
Nathan Wilcox <nejucomo@gmail.com>
2015-05-05 11:01:28 -0700

    Implement two of the "early" InvalidPreconditionTests which can be checked prior to seeing the wrapping function.

--- preconditions.py
+++ preconditions.py
@@ -1,8 +1,23 @@
+import inspect
+
+
 class PreconditionError (TypeError):
     pass
 
 
 def preconditions(*precs):
+
+    precinfo = []
+    for p in precs:
+        spec = inspect.getargspec(p)
+        if spec.varargs or spec.keywords:
+            raise PreconditionError(
+                'Precondition {!r} must not accept * nor ** args.'.format(p))
+
+        i = -len(spec.defaults)
+        appargs, closureargs = spec.args[:i], spec.args[i:]
+        precinfo.append( (appargs, closureargs, p) )
+
     def decorate(f):
         def g(*a, **kw):
             return f(*a, **kw)
