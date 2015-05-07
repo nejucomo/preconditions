aa3f325f71e6d1f7025fd597226a1fd495d21b80
Nathan Wilcox <nejucomo@gmail.com>
2015-05-07 09:02:57 -0700

    Add an MIT license.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name=PACKAGENAME,
     description='Flexible, concise preconditions.',
     url='https://github.com/nejucomo/{0}'.format(PACKAGENAME),
-    license='GPLv3',
+    license='MIT',
     version='0.1.dev0',
     author='Nathan Wilcox',
     author_email='nejucomo@gmail.com',
