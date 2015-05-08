d2699f79e544fdfee1745da00ad16a2950d6ee10
Nathan Wilcox <nejucomo@gmail.com>
2015-05-07 17:39:21 -0700

    Add a "signature requirement" to the sdist upload command.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
+import sys
 from setuptools import setup
+
+if 'upload' in sys.argv:
+    if '--sign' not in sys.argv and sys.argv[1:] != ['upload', '--help']:
+        raise SystemExit('Refusing to upload unsigned packages.')
 
 
 PACKAGENAME = 'preconditions'
